239d4e26a29565d1d450cc069d54359d4c29f73e
Shiping Yao <shiping.yao@shopee.com>
2023-10-09 12:53:04 +0800

    sync code

--- src/execution/radix_partitioned_hashtable.cpp
+++ src/execution/radix_partitioned_hashtable.cpp
@@ -657,16 +657,16 @@ void RadixHTLocalSourceState::Finalize(RadixHTGlobalSinkState &sink, RadixHTGlob
 		ht->ResetCount();
 	}
 
-	std::cout << "[Debug] partition data size: " << partition.data->Count() << std::endl;
 	// Now combine the uncombined data using this thread's HT
 	// NOTE: 这里的combine 还会需要分区吗，和partition.data->Combine有什么区别
+	// 这里的combine主要是合并Aggregate States(涉及聚合计算)
 	ht->Combine(*partition.data);
 	ht->UnpinData();
 
 	// Move the combined data back to the partition
 	partition.data =
 	    make_uniq<TupleDataCollection>(BufferManager::GetBufferManager(gstate.context), sink.radix_ht.GetLayout());
-	// NOTE: 这里为何也要Combine
+	// NOTE: 这里为何也要Combine, 传递给sink.partitions, 然后scan 用
 	partition.data->Combine(*ht->GetPartitionedData()->GetPartitions()[0]);
 
 	// Mark partition as ready to scan
